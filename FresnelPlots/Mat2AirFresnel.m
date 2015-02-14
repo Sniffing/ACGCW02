0c1c6df8d8950e2a5a67f139a3f56230eeb92e63
Terence Tse <tt1611@ray13.doc.ic.ac.uk>
2015-02-14 22:26:39 +0000

    Mat2Air Started

--- FresnelPlots/Mat2AirFresnel.m
+++ FresnelPlots/Mat2AirFresnel.m
@@ -1,7 +1,52 @@
-function [ output_args ] = Untitled( input_args )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [CriticalAngle] = Mat2AirFresnel()
+%Fresnal Reflection from Air(1.0) to Material (1.5)
+%   Fucntion will generate plots of Fresnal reflectance 
+%   for a dielectric material (index of refraction = 1.5)
+%   Plots out reflectance values for incidence angles
+%   in between 0 and 90 degrees. Must plot for
+%   parallel and perpendicular polarized components
+%   Also finds the reflectance of both components at
+%   normal incidence and the Brewster's angle.
 
+% Constant refraction indices
+refMat = 1.5; %nt
+refAir = 1.0; %ni
+
+% plot spacing
+incidenceAngles = linspace(0,pi/2,100);
+transmissionAngles = getTransAngles(incidenceAngles,refAir,refMat);
+
+parComponent = parReflectance(incidenceAngles,transmissionAngles,refAir,refMat);
+
+perpComponent = perpReflectance(incidenceAngles,transmissionAngles,refAir,refMat);
+
+figure
+plot(incidenceAngles,parComponent, incidenceAngles, perpComponent)
 
 end
+
+%Works out transmission angles from Snell's equation
+function transmissionAngles = getTransAngles(incidenceAngles,refAir,refMat)
+    transmissionAngles= zeros(1,size(incidenceAngles,2));
+    for i = 1:size(incidenceAngles,2)
+        snells = refAir*sin(incidenceAngles(i));
+        transmissionAngles(i) = asin(snells/refMat);
+    end
+end
+
+%Fresnel Reflectance for parallel polarized light
+function reflectance = parReflectance(incidenceAngles,transmissionAngles,refAir,refMat)
+    airPar = refAir * cos(transmissionAngles);
+    materialPar = refMat * cos(incidenceAngles);
+    reflectance = abs((materialPar-airPar) ./ (materialPar+airPar));
+end
+
+%Fresnel Reflectance for perpendicular polarized light
+function reflectance = perpReflectance(incidenceAngles,transmissionAngles,refAir,refMat)
+    airPerp = refAir * cos(incidenceAngles);
+    materialPerp = refMat * cos(transmissionAngles);
+    reflectance = abs((airPerp-materialPerp) ./ (airPerp+materialPerp));
+end
+
+
 
