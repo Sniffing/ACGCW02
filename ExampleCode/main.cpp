afa6e47f0bf1277c74e5abbf84c3caaf9a2053ab
jutinko <jutkko@gmail.com>
2015-02-25 13:46:27 +0000

    Changed for sampling and rendering sphere

--- ExampleCode/main.cpp
+++ ExampleCode/main.cpp
@@ -17,13 +17,15 @@ int main(int argc, char** argv)
   unsigned int width, height, numComponents;
   float* img_in = loadPFM(argv[1], width, height, numComponents);
   EM myEM(img_in, width, height, numComponents);
-  vector<pair<int, int> > samples;
-  myEM.sample(samples, 1024);
-  for(size_t i = 0; i < samples.size(); ++i)
-  {
-    myEM.color(samples[i].first, samples[i].second);
-  }
-  WritePFM(argv[2], width, height, numComponents, img_in);
+  //for(size_t i = 0; i < samples.size(); ++i)
+  //{
+  //  myEM.color(samples[i].first, samples[i].second);
+  //}
+
+  //WritePFM(argv[2], width, height, numComponents, img_in);
+  float* sphere_in = loadPFM(argv[2], width, height, numComponents);
+
+
 
   return 0;
 }
